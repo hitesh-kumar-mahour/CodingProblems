d7b4341faaa1422e6e666e0be8f411df5a154e34
mahour <mahour@adobe.com>
2023-05-10 00:35:08 +0530

    nodes at K

--- src/gfg/NodesAtDistanceK.java
+++ src/gfg/NodesAtDistanceK.java
@@ -8,61 +8,61 @@ class NodeA{
 public class NodesAtDistanceK {
 
     public static void KDistanceNodesDown(NodeA root, int k, ArrayList<Integer>ans){
-        
+
         if(root==null || k<0)
             return;
         if(k==0){
             ans.add(root.data);
             return;
         }
-        
+
         KDistanceNodesDown(root.left,k-1,ans);
         KDistanceNodesDown(root.right,k-1,ans);
-        
+
     }
 
     public static int KDistanceNodes(NodeA root,int target, int k, ArrayList<Integer> ans){
-        if(root==null)
-            return -1; 
-            
+        if(root==null || k<0)
+            return -1;
+
         if(root.data == target){
             KDistanceNodesDown(root,k,ans);
-            return 0;
+            return k-1;
         }
-            
+
         int l = KDistanceNodes(root.left,target,k,ans);
         if(l>=0){
-            if(l+1==k)
+            if(l==0)
                 ans.add(root.data);
             else
-                KDistanceNodesDown(root.right,k-2-l,ans);
-            return l+1;
+                KDistanceNodesDown(root.right,l-1,ans);
+            return l-1;
         }
-        
+
         int r = KDistanceNodes(root.right,target,k,ans);
         if(r>=0){
-            if(r+1==k)
+            if(r==0)
                 ans.add(root.data);
             else
-                KDistanceNodesDown(root.left,k-2-r,ans);
-            return r+1;
+                KDistanceNodesDown(root.left,r-1,ans);
+            return r-1;
         }
 
         return -1;
     }
-    
-    
+
+
     public static ArrayList<Integer> KDistanceNodes(NodeA root, int target , int k){
 
         ArrayList<Integer> ans = new ArrayList<>();
 
         if(root==null || k<0)
             return ans;
-        
+
         KDistanceNodes(root,target,k,ans);
         Collections.sort(ans);
         return ans;
 
     }
-	
+
 }
--- src/lec21_PriorityQ/HeapSort.java
+++ src/lec21_PriorityQ/HeapSort.java
@@ -1,73 +1,69 @@
 package lec21_PriorityQ;
 
+import java.util.Arrays;
+
 public class HeapSort {
-public static void insertInVirtualHeap(int[] input, int index){
-		
-		int childIndex = index;
-		int parentIndex = (childIndex - 1) / 2;
-		while(childIndex > 0){
-			if(input[childIndex] < input[parentIndex]){
-				int temp = input[childIndex];
-				input[childIndex] = input[parentIndex];
-				input[parentIndex] = temp;
-				childIndex = parentIndex;
-				parentIndex =  (childIndex - 1) / 2;	
-			}else{
-				return;
-			}
-		}
-	}
-private static int removeFromVirtualHeap(int[] input, int size){
-	// TODO Auto-generated method stub
-	int temp = input[0];
-	input[0] = input[size - 1];
-	size--;
-	if(size > 1){
-		downHeapify(input,size,0);
-	}
-	return temp;
-}
-	private static void downHeapify(int[] input, int size, int index) {
-		int leftChildIndex = 2*index + 1;
-		int rightChildIndex = 2*index + 2;
-		if(leftChildIndex > size - 1){
-			return;
-		}
-		int minIndex = index;
-		if(input[leftChildIndex] < input[minIndex]){
-			minIndex = leftChildIndex;
-		}
-		if(rightChildIndex < size && input[rightChildIndex] < input[minIndex]){
-			minIndex = rightChildIndex;
-		}
-		if(minIndex != index){
-			int temp = input[minIndex];
-			input[minIndex] = input[index];
-			input[index] = temp;
-			downHeapify(input, size, minIndex);
-		}
-}
+    public static void insertInVirtualHeap(int[] input, int index) {
 
-	//public static void heapSort(int []input)
-	{
-		
-	}
-	public static void main(String[] args) {
+        int childIndex = index;
+        int parentIndex = ( childIndex - 1 ) / 2;
+        while (childIndex > 0) {
+            if (input[childIndex] < input[parentIndex]) {
+                int temp = input[childIndex];
+                input[childIndex] = input[parentIndex];
+                input[parentIndex] = temp;
+                childIndex = parentIndex;
+                parentIndex = ( childIndex - 1 ) / 2;
+            } else {
+                return;
+            }
+        }
+    }
 
-		int []input={6,4,7,5,3};
-		for(int i = 0; i < input.length; i++){
-			insertInVirtualHeap(input, i);
-		}
-//for(int i:input)
-//	System.out.println(i);
-//	}
-		for(int i = 0; i < input.length; i++){
-			 input[input.length - i - 1]  = removeFromVirtualHeap(input, input.length - i);
-			
-		}
-		for(int i : input){
-			System.out.println(i);
-		}
-			
-	}
+    private static int removeFromVirtualHeap(int[] input, int size) {
+        int temp = input[0];
+        input[0] = input[size - 1];
+        size--;
+        if (size > 1) {
+            downHeapify(input, size, 0);
+        }
+        return temp;
+    }
+
+    private static void downHeapify(int[] input, int size, int index) {
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+        if (leftChildIndex > size - 1) {
+            return;
+        }
+        int minIndex = index;
+        if (input[leftChildIndex] < input[minIndex]) {
+            minIndex = leftChildIndex;
+        }
+        if (rightChildIndex < size && input[rightChildIndex] < input[minIndex]) {
+            minIndex = rightChildIndex;
+        }
+        if (minIndex != index) {
+            int temp = input[minIndex];
+            input[minIndex] = input[index];
+            input[index] = temp;
+            downHeapify(input, size, minIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] input = {6, 4, 7, 5, 3};
+        for (int i = 0; i < input.length; i++) {
+            insertInVirtualHeap(input, i);
+        }
+        System.out.println("Heap\t" + Arrays.toString(input));
+
+        for (int i = 0; i < input.length; i++) {
+            input[input.length - i - 1] = removeFromVirtualHeap(input, input.length - i);
+
+        }
+        System.out.println("Sorted\t" + Arrays.toString(input));
+
+    }
 }
